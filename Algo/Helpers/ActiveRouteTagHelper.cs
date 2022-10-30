9904899f565eb87b98c32e8f2facd8bb7b6aeb36
xhette <margotcup@gmail.com>
2022-10-30 18:43:14 +0300

    *

--- Algo/Helpers/ActiveRouteTagHelper.cs
+++ Algo/Helpers/ActiveRouteTagHelper.cs
@@ -1,100 +1,109 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.Routing;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
-[HtmlTargetElement(Attributes = "is-active-route")]
-public class ActiveRouteTagHelper : TagHelper
+namespace Algo.Helpers
 {
-    private IDictionary<string, string> _routeValues;
-
-    /// <summary>The name of the action method.</summary>
-    /// <remarks>Must be <c>null</c> if <see cref="P:Microsoft.AspNetCore.Mvc.TagHelpers.AnchorTagHelper.Route" /> is non-<c>null</c>.</remarks>
-    [HtmlAttributeName("asp-action")]
-    public string Action { get; set; }
-
-    /// <summary>The name of the controller.</summary>
-    /// <remarks>Must be <c>null</c> if <see cref="P:Microsoft.AspNetCore.Mvc.TagHelpers.AnchorTagHelper.Route" /> is non-<c>null</c>.</remarks>
-    [HtmlAttributeName("asp-controller")]
-    public string Controller { get; set; }
-
-    /// <summary>Additional parameters for the route.</summary>
-    [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
-    public IDictionary<string, string> RouteValues
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("a")]
+    public class ActiveRouteTagHelper : TagHelper
     {
-        get
+        private IDictionary<string, string> _routeValues;
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+
+        [HtmlAttributeName("active-route-check")]
+        public bool IsActiveRouteCheck { get; set; }
+
+
+        /// <summary>Additional parameters for the route.</summary>
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public IDictionary<string, string> RouteValues
         {
-            if (this._routeValues == null)
-                this._routeValues = (IDictionary<string, string>)new Dictionary<string, string>((IEqualityComparer<string>)StringComparer.OrdinalIgnoreCase);
-            return this._routeValues;
-        }
-        set
-        {
-            this._routeValues = value;
-        }
-    }
-
-    /// <summary>
-    /// Gets or sets the <see cref="T:Microsoft.AspNetCore.Mvc.Rendering.ViewContext" /> for the current request.
-    /// </summary>
-    [HtmlAttributeNotBound]
-    [ViewContext]
-    public ViewContext ViewContext { get; set; }
-
-    public override void Process(TagHelperContext context, TagHelperOutput output)
-    {
-        base.Process(context, output);
-
-        if (ShouldBeActive())
-        {
-            MakeActive(output);
-        }
-
-        output.Attributes.RemoveAll("is-active-route");
-    }
-
-    private bool ShouldBeActive()
-    {
-        string currentController = ViewContext.RouteData.Values["Controller"].ToString();
-        string currentAction = ViewContext.RouteData.Values["Action"].ToString();
-
-        if (!string.IsNullOrWhiteSpace(Controller) && Controller.ToLower() != currentController.ToLower())
-        {
-            return false;
-        }
-
-        if (!string.IsNullOrWhiteSpace(Action) && Action.ToLower() != currentAction.ToLower())
-        {
-            return false;
-        }
-
-        foreach (KeyValuePair<string, string> routeValue in RouteValues)
-        {
-            if (!ViewContext.RouteData.Values.ContainsKey(routeValue.Key) ||
-                ViewContext.RouteData.Values[routeValue.Key].ToString() != routeValue.Value)
+            get
             {
-                return false;
+                if (this._routeValues == null)
+                    this._routeValues = (IDictionary<string, string>)new Dictionary<string, string>((IEqualityComparer<string>)StringComparer.OrdinalIgnoreCase);
+                return this._routeValues;
+            }
+            set
+            {
+                this._routeValues = value;
             }
         }
 
-        return true;
-    }
+        /// <summary>
+        /// Gets or sets the <see cref="T:Microsoft.AspNetCore.Mvc.Rendering.ViewContext" /> for the current request.
+        /// </summary>
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
 
-    private void MakeActive(TagHelperOutput output)
-    {
-        var classAttr = output.Attributes.FirstOrDefault(a => a.Name == "class");
-        if (classAttr == null)
+        public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            classAttr = new TagHelperAttribute("class", "active");
-            output.Attributes.Add(classAttr);
+            base.Process(context, output);
+
+            if (ShouldBeActive())
+            {
+                MakeActive(output);
+            }
+
+            output.Attributes.RemoveAll("is-active-route");
         }
-        else if (classAttr.Value == null || classAttr.Value.ToString().IndexOf("active") < 0)
+
+        private bool ShouldBeActive()
         {
-            output.Attributes.SetAttribute("class", classAttr.Value == null
-                ? "active"
-                : classAttr.Value.ToString() + " active");
+            if (!IsActiveRouteCheck)
+            {
+                return false;
+            }
+
+            string currentController = ViewContext.RouteData.Values["Controller"].ToString();
+            string currentAction = ViewContext.RouteData.Values["Action"].ToString();
+
+            if (!string.IsNullOrWhiteSpace(Controller) && Controller.ToLower() != currentController.ToLower())
+            {
+                return false;
+            }
+
+            if (!string.IsNullOrWhiteSpace(Action) && Action.ToLower() != currentAction.ToLower())
+            {
+                return false;
+            }
+
+            foreach (KeyValuePair<string, string> routeValue in RouteValues)
+            {
+                if (!ViewContext.RouteData.Values.ContainsKey(routeValue.Key) ||
+                    ViewContext.RouteData.Values[routeValue.Key].ToString() != routeValue.Value)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void MakeActive(TagHelperOutput output)
+        {
+            var classAttr = output.Attributes.FirstOrDefault(a => a.Name == "class");
+            if (classAttr == null)
+            {
+                classAttr = new TagHelperAttribute("class", "active");
+                output.Attributes.Add(classAttr);
+            }
+            else if (classAttr.Value == null || classAttr.Value.ToString().IndexOf("active") < 0)
+            {
+                output.Attributes.SetAttribute("class", classAttr.Value == null
+                    ? "active"
+                    : classAttr.Value.ToString() + " active");
+            }
         }
     }
 }
