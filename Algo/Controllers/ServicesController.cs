6e98669ed0b882d9650cede22b6f6170fccb4a72
xhette <margotcup@gmail.com>
2022-11-07 22:04:54 +0300

    *

---
+++ Algo/Controllers/ServicesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Algo.Views.Shared
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Development()
+        {
+            return View();
+        }
+
+        public IActionResult Integration()
+        {
+            return View();
+        }
+
+        public IActionResult Support()
+        {
+            return View();
+        }
+
+        public IActionResult Monitoring()
+        {
+            return View();
+        }
+
+        public IActionResult Dispatching()
+        {
+            return View();
+        }
+
+        public IActionResult Alerts()
+        {
+            return View();
+        }
+    }
+}
