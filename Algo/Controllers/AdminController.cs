8232a6582fa1752c55a506771976dce624e6ec03
xhette <margotcup@gmail.com>
2022-11-15 21:16:23 +0300

    *

---
+++ Algo/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Algo.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Login));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ResetPassword(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Login));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
